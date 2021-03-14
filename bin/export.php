daf94dd8f925c5e35f9bf8d92b5ff635f0a98833
ryunosuke <ryunosuke.arima@gmail.com>
2021-03-14 20:25:38 +0900

    [composer] build

--- bin/export.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ryunosuke\Documentize;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-file_put_contents(__DIR__ . "/../src/functions.php", \ryunosuke\Functions\Transporter::exportNamespace(__NAMESPACE__));
